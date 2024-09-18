eae0cc283496faba71cd39c6f460b09378d5f994
flashrifle <flashrifle@gmail.com>
2024-09-18 22:20:25 +0900

    FEAT: 하드코딩 된 select option value 리스트 변수로 수정

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,13 +2,20 @@
 import './main.css';
 import Icon from '../../../public/icons/icon';
 import { useState } from 'react';
+import { DifficultyData, LanguageData } from '@/components/main/mainData';
 
 export default function Main() {
   const [lang, setLang] = useState<string>('');
   const [difficulty, setDifficulty] = useState<string>('');
   const [count, setCount] = useState<number>();
 
+  const allowedLangs = LanguageData;
+  const allowedDifficulties = DifficultyData;
+
   const handleClickBtn = () => {
+    if (!lang || !difficulty || !count) {
+      alert('선택하지 않은 항목이 있습니다.');
+    }
     console.log(lang, difficulty, count);
   };
 
@@ -22,16 +29,19 @@ export default function Main() {
         <div className="lang-form">
           <select name="" id="" onChange={(e) => setLang(e.target.value)}>
             <option>언어를 선택해 주세요</option>
-            <option>Javascript</option>
-            <option>SQL</option>
+            {allowedLangs.map((item, idx) => (
+              <option key={idx} value={item}>
+                {item}
+              </option>
+            ))}
           </select>
         </div>
         <div className="difficulty-form">
           <select name="" id="" onChange={(e) => setDifficulty(e.target.value)}>
             <option>난이도를 선택해 주세요</option>
-            <option>고급</option>
-            <option>중급</option>
-            <option>초급</option>
+            {allowedDifficulties.map((item, idx) => (
+              <option key={idx}>{item}</option>
+            ))}
           </select>
         </div>
         <input
---
+++ src/components/main/mainData.ts
@@ -0,0 +1,14 @@
+export const LanguageData = [
+  'Java',
+  'JavaScript',
+  'Python',
+  'C',
+  'C#',
+  'C++',
+  'SQL',
+  'MySQL',
+  'MS-SQL',
+  'PostgreSQL',
+];
+
+export const DifficultyData = ['초급', '중급', '고급'];
