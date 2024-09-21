08b736373ff0b3ffbadd114e1c7f94dc3050da21
flashrifle <flashrifle@gmail.com>
2024-09-21 14:37:41 +0900

    FEAT: RecoilRoot 배치

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import './page.css';
 import Footer from '@/components/footer/Footer';
 import Main from '@/components/main/Main';
+import { RecoilRoot } from 'recoil';
 
 export default function Home() {
   return (
-    <div className="content-container">
-      <Main />
-      <Footer />
-    </div>
+    <RecoilRoot>
+      <div className="content-container">
+        <Main />
+        <Footer />
+      </div>
+    </RecoilRoot>
   );
 }
