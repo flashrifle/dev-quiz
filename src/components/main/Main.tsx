9ca5b15b176701304d9410bf64f802ea26d2a7ba
flashrifle <flashrifle@gmail.com>
2024-09-27 16:28:07 +0900

    FEAT: 로딩 UI 구현

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -7,11 +7,13 @@ import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { useRecoilState } from 'recoil';
 import { examDataStore } from '../../../recoil/store';
+import { BarLoader, ClipLoader } from 'react-spinners';
 
 export default function Main() {
   const [lang, setLang] = useState<string>('');
   const [difficulty, setDifficulty] = useState<string>('');
   const [count, setCount] = useState<number>();
+  const [loading, setLoading] = useState<boolean>(false);
   const [responseData, setResponseData] = useRecoilState(examDataStore); // 응답 데이터를 저장할 상태
   const router = useRouter();
 
@@ -25,6 +27,7 @@ export default function Main() {
       alert('선택하지 않은 항목이 있습니다.');
       return;
     }
+    setLoading(true);
     await axios
       .post(`${host}/api/examCreate`, {
         lang: lang,
@@ -32,52 +35,63 @@ export default function Main() {
         count: count,
       })
       .then((res) => {
+        setLoading(false);
         setResponseData(res.data);
-        console.log(res.data);
         router.push('/api/examCreate');
       })
       .catch((err) => console.error(err));
   };
 
   return (
-    <div className="main-container">
-      <div className="title-container">
-        <Icon />
-        <p className="main-title">프로그래밍 문제 생성기</p>
-      </div>
-      <div className="main-form-container">
-        <div className="lang-form">
-          <select name="" id="" onChange={(e) => setLang(e.target.value)}>
-            <option>언어를 선택해 주세요</option>
-            {allowedLangs.map((item, idx) => (
-              <option key={idx} value={item}>
-                {item}
-              </option>
-            ))}
-          </select>
+    <>
+      {loading && (
+        <div className="loading-wrap">
+          <ClipLoader size={80} color={'#47A289'} />
         </div>
-        <div className="difficulty-form">
-          <select name="" id="" onChange={(e) => setDifficulty(e.target.value)}>
-            <option>난이도를 선택해 주세요</option>
-            {allowedDifficulties.map((item, idx) => (
-              <option key={idx}>{item}</option>
-            ))}
-          </select>
+      )}
+      <div className="main-container">
+        <div className="title-container">
+          <Icon />
+          <p className="main-title">프로그래밍 문제 생성기</p>
         </div>
-        <input
-          type="text"
-          placeholder="문항수를 입력하세요"
-          onChange={(e) => setCount(parseInt(e.target.value))}
-        />
-        <button onClick={handleClickBtn}>생성하기</button>
+        <div className="main-form-container">
+          <div className="lang-form">
+            <select name="" id="" onChange={(e) => setLang(e.target.value)}>
+              <option>언어를 선택해 주세요</option>
+              {allowedLangs.map((item, idx) => (
+                <option key={idx} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="difficulty-form">
+            <select
+              name=""
+              id=""
+              onChange={(e) => setDifficulty(e.target.value)}
+            >
+              <option>난이도를 선택해 주세요</option>
+              {allowedDifficulties.map((item, idx) => (
+                <option key={idx}>{item}</option>
+              ))}
+            </select>
+          </div>
+          <input
+            type="text"
+            placeholder="문항수를 입력하세요"
+            onChange={(e) => setCount(parseInt(e.target.value))}
+          />
+          <button onClick={handleClickBtn}>생성하기</button>
+        </div>
+        {/*<hr style={{ width: 1920 }} />*/}
+        {/*{responseData && (*/}
+        {/*  <div className="response-container">*/}
+        {/*    <p>응답 결과:</p>*/}
+        {/*    <pre>{JSON.stringify(responseData, null, 2)}</pre>*/}
+        {/*  </div>*/}
+        {/*)}*/}
       </div>
-      {/*<hr style={{ width: 1920 }} />*/}
-      {/*{responseData && (*/}
-      {/*  <div className="response-container">*/}
-      {/*    <p>응답 결과:</p>*/}
-      {/*    <pre>{JSON.stringify(responseData, null, 2)}</pre>*/}
-      {/*  </div>*/}
-      {/*)}*/}
-    </div>
+    </>
   );
 }
